ebd8dccdb7d6e9024621313b1940a367cc7de089
yaniv2809 <yaniv2809@gmail.com>
2026-02-23 20:57:59 +0200

    update logger to be singleton and create dynamic log file name based on current date and time

---
+++ config/config.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+class ConfigManager:
+    CONFIG_FILE_PATH = os.path.join(os.path.dirname(__file__), 'config.json')
+
+    @staticmethod
+    def get_config():
+        try:
+            with open(ConfigManager.CONFIG_FILE_PATH, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            raise Exception(f"Configuration file not found at: {ConfigManager.CONFIG_FILE_PATH}")
+        except json.JSONDecodeError:
+            raise Exception("Error decoding JSON from the configuration file. Please check the JSON format.")
+
+    @staticmethod
+    def get_env_data():
+        config = ConfigManager.get_config()
+        active_env = config.get("active_env")
+
+        return config.get("environments").get(active_env)
---
+++ extensions/api_actions.py
@@ -0,0 +1,41 @@
+import requests
+from utils.logger import Logger
+
+class APIActions:
+    """
+    מחלקה זו עוטפת את פעולות ה-API הבסיסיות (GET, POST, PUT, DELETE).
+    היא משלבת את הלוגר שלנו כדי שכל בקשה שנשלחת ותשובה שמתקבלת יתועדו אוטומטית!
+    """
+    @staticmethod
+    def get(url):
+        log = Logger()
+        log.info(f"API Action: Sending GET request to {url}")
+        response = requests.get(url)
+        log.info(f"API Action: Received response with status code {response.status_code}")
+        return response
+
+    @staticmethod
+    def post(url, payload):
+        log = Logger()
+        log.info(f"API Action: Sending POST request to {url} with payload: {payload}")
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url, json=payload, headers=headers)
+        log.info(f"API Action: Received response with status code {response.status_code}")
+        return response
+
+    @staticmethod
+    def put(url, payload):
+        log = Logger()
+        log.info(f"API Action: Sending PUT request to {url} with payload: {payload}")
+        headers = {'Content-Type': 'application/json'}
+        response = requests.put(url, json=payload, headers=headers)
+        log.info(f"API Action: Received response with status code {response.status_code}")
+        return response
+
+    @staticmethod
+    def delete(url):
+        log = Logger()
+        log.info(f"API Action: Sending DELETE request to {url}")
+        response = requests.delete(url)
+        log.info(f"API Action: Received response with status code {response.status_code}")
+        return response
