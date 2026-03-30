027f61a5391deb9ed2d47d34d5eb544e92c1eff5
yaniv2809 <yaniv2809@gmail.com>
2026-03-30 17:12:11 +0300

    config/config.py updated with new configuration settings.

--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+
 class ConfigManager:
     CONFIG_FILE_PATH = os.path.join(os.path.dirname(__file__), "config.json")
     PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -9,24 +10,34 @@ class ConfigManager:
     def get_config():
         try:
             with open(ConfigManager.CONFIG_FILE_PATH, 'r', encoding='utf-8') as file:
-                return json.load(file)
+                config = json.load(file)
         except FileNotFoundError:
             raise Exception(f"Configuration file not found at: {ConfigManager.CONFIG_FILE_PATH}")
         except json.JSONDecodeError:
             raise Exception("Error decoding JSON from the configuration file. Please check the JSON format.")
 
+        config["db_type"] = os.environ.get("DB_TYPE", config.get("db_type", "sqlite"))
+        config["headless"] = os.environ.get("HEADLESS", str(config.get("headless", False))).lower() == "true"
+        return config
+
     @staticmethod
     def get_env_data():
         config = ConfigManager.get_config()
         active_env = config.get("active_env")
+        env_data = config.get("environments", {}).get(active_env, {})
 
-        return config.get("environments").get(active_env)
-    
+        env_data["web_url"] = os.environ.get("WEB_URL", env_data.get("web_url"))
+        env_data["api_url"] = os.environ.get("API_URL", env_data.get("api_url"))
+        env_data["flask_api_url"] = os.environ.get("FLASK_API_URL", env_data.get("flask_api_url"))
+        env_data["ai_url"] = os.environ.get("AI_URL", env_data.get("ai_url"))
+        env_data["currency_api_url"] = os.environ.get("CURRENCY_API_URL", env_data.get("currency_api_url"))
+
+        return env_data
+
     @staticmethod
     def get_performance_config():
         config = ConfigManager.get_config()
         return config.get("performance")
-    
 
     @staticmethod
     def get_db_path():
